2fc3de17aaf3c0c4585305dd84e496b2e42b4b1b
Stervger <1406421751@qq.com>
2019-10-25 17:13:29 +0800

    2019/10/25

--- companyinfo/spiders/qixinbao.py
+++ companyinfo/spiders/qixinbao.py
@@ -1,45 +1,39 @@
 # -*- coding: utf-8 -*-
-import urllib
-from companyinfo.items import CompanyinfoItem
+import codecs
+from selenium import webdriver
+import time
 import scrapy
-
+import re
 
 class QixinbaoSpider(scrapy.Spider):
-    cookie = 'acw_tc=707c9fdb15710509288834359e54c6057047891e03cbb554e69db2c9fd791d; channel=%2Bbaidusem17; Hm_lvt_52d64b8d3f6d42a2e416d59635df3f71=1571050927; Hm_lpvt_52d64b8d3f6d42a2e416d59635df3f71=1571052800; sid=s%3A5zX7X9rzI7wcIjvPmB3BaGfxNml6PFRc.tL%2B%2BQ1GOQOQFrZqwZnQom7Z9928lxFadVD9zHy2VdQU'
+    cookie = 'acw_tc=2f624a5815717107809494134e0454956bde8fdbcc0c1368190122d159d980; channel=%2Bbaidusem17; cookieShowLoginTip=3; Hm_lvt_52d64b8d3f6d42a2e416d59635df3f71=1571882386,1571966085,1571992694,1571994259; acw_sc__v2=5db2ba998709f844b8f9ed3c7809e0149ca525e3; sid=s%3ABFXg0UgU7QMjQRfEsH8ciwXdYc57XzDN.WXP4OtjBJ9lVAV29GfEUOguzn95fe99velAlXHrtn1U; Hm_lpvt_52d64b8d3f6d42a2e416d59635df3f71=1571994290'
     headers = {
         'Host': 'www.qixin.com',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36',
         'Cookie': cookie
     }
 
     name = 'qixinbao'
-    allowed_domains = ['https://www.qixin.com']
-    start_urls = ['https://www.qixin.com/search?from=baidusem17&key=']
 
     def start_requests(self):
-        # 查询公司
-        f = open('./company_list.txt', 'r', encoding='utf-8')
-        for link in f:
-            company = urllib.parse.quote(link).replace('\n', '')
-            url = self.start_urls[0] + company
-            url = url.replace('%0A','&page=1')
-            print(url)
-            yield scrapy.Request(url, headers=self.headers, callback=self.parse)
-
+        f = open('./2.txt','r',encoding='utf-8').readlines()
+        for a2 in f:
+            urls = ['https://www.qixin.com/search?from=baidusem24&key=重庆'+str(a2)[:-1]+'&page={0}'.format(i) for i in range(1,4)]
+            for url in urls:
+                yield scrapy.Request(url=str(url),headers=self.headers,callback=self.parse)
+                print(url)
     def parse(self, response):
-        # 提取列表中第一个公司，进入该页
-        link = response.css('.company-title a::attr(href)').extract()
-        detail_link = response.urljoin(link)
-        print(detail_link)
-        yield scrapy.Request(detail_link, headers=self.headers, callback=self.detail)
+        driver = webdriver.Chrome()
+        driver.get(response.url)
+        time.sleep(20)
+        driver.close()
+        urls = re.findall(r'keyId="(.*?)"',response.text,re.M)
+        print(urls)
+        file = codecs.open('company.txt', 'a+', encoding='utf-8')
+        for url in urls:
+            file.write(url+'\n')
+        file.close()
 
-    def detail(self, response):
-        items = CompanyinfoItem()
-
-        licence_pro = response.xpath('/html/body/div[5]/div/div[1]/div[1]/table/tbody/tr[3]/td[2]//text()').extract()[0]
-        items['licence_pro'] = licence_pro.strip().replace('\n', '')
-
-        yield items
 
 
 
--- companyinfo/spiders/trademark.py
+++ companyinfo/spiders/trademark.py
@@ -4,6 +4,7 @@ import time
 from selenium import webdriver
 from companyinfo.items import CompanyinfoItem
 
+
 class TrademarkSpider(scrapy.Spider):
     headers = {
         'Host': 'www.qixin.com',
@@ -12,48 +13,89 @@ class TrademarkSpider(scrapy.Spider):
     name = 'trademark'
     start_urls = {'https://www.qixin.com/ability/60214084-2b87-4ea3-9131-e4c313deacf0'}
     def parse(self, response):
-        drive = webdriver.Firefox()
+        drive = webdriver.Chrome()
         drive.get("https://www.qixin.com/ability/60214084-2b87-4ea3-9131-e4c313deacf0")
         items = CompanyinfoItem()
 
-        #商标
-        # sml = {}
-        # i=1
-        # a=[]
-        # tr_list = response.xpath('//*[@id="trademark"]/table/tbody/tr')
-        # for tr in tr_list:
-        #     detail = drive.find_element_by_xpath('//table[@class="table table-bordered margin-t-1x text-middle"]/tbody/tr[{0}]/td[@class="text-center nowrap"]/a'.format(i))
-        #     drive.execute_script('arguments[0].click()', detail)
-        #     time.sleep(2)
-        #     sml['商标图片']=drive.find_element_by_xpath('//img[@class="img-responsive img-center"]').get_attribute('src')
-        #     sml['商标名称']=drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr/td[@class="td-2"]').get_attribute('textContent')
-        #     sml['商标注册号']=drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr[2]/td[2]').get_attribute('textContent')
-        #     sml['商标类别']=drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr[3]/td[2]').get_attribute('textContent')
-        #     sml['商标状态']=drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr[4]/td[2]').get_attribute('textContent')
-        #     sml['申请人']=drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[5]/td[2]/a').get_attribute('href')
-        #     sml['申请日']=drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[6]/td[2]').get_attribute('textContent')
-        #     sml['初审公告日']=drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[6]/td[4]').get_attribute('textContent')
-        #     sml['注册公告日']=drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[7]/td[2]').get_attribute('textContent')
-        #     sml['专用期限']=drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[7]/td[4]').get_attribute('textContent')
-        #     sml['专利代理机构']=drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[8]/td[2]/a').get_attribute('href')
-        #     sml['商标公告']=drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[9]/td[2]').get_attribute('textContent')
-        #     sml['商品服务项目']=drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[11]/td[1]').get_attribute('textContent')
-        #     close = drive.find_element_by_xpath('//div[@class="modal-header modal-new-header"]/div[@class="close-div"]')
-        #     drive.execute_script('arguments[0].click()', close)
-        #     time.sleep(1)
-        #     a.append({i:{'商标图片':sml['商标图片'],'商标名称':sml['商标名称'],'商标注册号':sml['商标注册号'],'商标类别':sml['商标类别'],'商标状态':sml['商标状态'],'申请人':sml['申请人'],'申请日':sml['申请日'],'初审公告日':sml['初审公告日'],'注册公告日':sml['注册公告日'],'专用期限':sml['专用期限'],'专利代理机构':sml['专利代理机构'],'商标公告':sml['商标公告'],'商品服务项目':sml['商品服务项目']}})
-        #     i+=1
-        # print(a)
-        # items['商标']=a
+        totalpage = int(drive.find_element_by_xpath('//*[@id="trademark"]/div[1]/h4/span').get_attribute('textContent'))//5 + 1
+        print(totalpage)
+        if totalpage!=1:
+            for p in range(1,totalpage):
+                if p <= 3:
+                    p=p
+                elif p==totalpage:
+                    p=5
+                else:
+                    p=4
+                page = drive.find_element_by_xpath('//*[@id="trademark"]/div[2]/div/nav/ul/li[{0}]/a'.format(p+1))
+                drive.execute_script('arguments[0].click()', page)
+                time.sleep(2)
+                sml = {}
+                i = 1
+                a = []
+                tr_list = response.xpath('//*[@id="trademark"]/table/tbody/tr')
+                for tr in tr_list:
+                    detail = drive.find_element_by_xpath('//table[@class="table table-bordered margin-t-1x text-middle"]/tbody/tr[{0}]/td[@class="text-center nowrap"]/a'.format(i))
+                    drive.execute_script('arguments[0].click()', detail)
+                    time.sleep(2)
+                    sml['商标图片'] = drive.find_element_by_xpath('//img[@class="img-responsive img-center"]').get_attribute('src')
+                    sml['商标名称'] = drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr/td[@class="td-2"]').get_attribute('textContent')
+                    sml['商标注册号'] = drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr[2]/td[2]').get_attribute('textContent')
+                    sml['商标类别'] = drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr[3]/td[2]').get_attribute('textContent')
+                    sml['商标状态'] = drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr[4]/td[2]').get_attribute('textContent')
+                    sml['申请人'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[5]/td[2]').get_attribute('textContent')
+                    sml['申请日'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[6]/td[2]').get_attribute('textContent')
+                    sml['初审公告日'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[6]/td[4]').get_attribute('textContent')
+                    sml['注册公告日'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[7]/td[2]').get_attribute('textContent')
+                    sml['专用期限'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[7]/td[4]').get_attribute('textContent')
+                    sml['专利代理机构'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[8]/td[2]').get_attribute('textContent')
+                    sml['商标公告'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[9]/td[2]').get_attribute('textContent')
+                    sml['商品服务项目'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[11]/td[1]').get_attribute('textContent')
+                    close = drive.find_element_by_xpath('//div[@class="modal-header modal-new-header"]/div[@class="close-div"]')
+                    drive.execute_script('arguments[0].click()', close)
+                    a.append({i: {'商标图片': sml['商标图片'], '商标名称': sml['商标名称'], '商标注册号': sml['商标注册号'], '商标类别': sml['商标类别'],'商标状态': sml['商标状态'], '申请人': sml['申请人'], '申请日': sml['申请日'], '初审公告日': sml['初审公告日'],'注册公告日': sml['注册公告日'], '专用期限': sml['专用期限'], '专利代理机构': sml['专利代理机构'],'商标公告': sml['商标公告'], '商品服务项目': sml['商品服务项目']}})
+                    i += 1
+                print(a)
+                items['商标'] = a
+                yield items
+        else:
+            sml = {}
+            i = 1
+            a = []
+            tr_list = response.xpath('//*[@id="trademark"]/table/tbody/tr')
+            for tr in tr_list:
+                detail = drive.find_element_by_xpath('//table[@class="table table-bordered margin-t-1x text-middle"]/tbody/tr[{0}]/td[@class="text-center nowrap"]/a'.format(i))
+                drive.execute_script('arguments[0].click()', detail)
+                time.sleep(2)
+                sml['商标图片'] = drive.find_element_by_xpath('//img[@class="img-responsive img-center"]').get_attribute('src')
+                sml['商标名称'] = drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr/td[@class="td-2"]').get_attribute('textContent')
+                sml['商标注册号'] = drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr[2]/td[2]').get_attribute('textContent')
+                sml['商标类别'] = drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr[3]/td[2]').get_attribute('textContent')
+                sml['商标状态'] = drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr[4]/td[2]').get_attribute('textContent')
+                sml['申请人'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[5]/td[2]').get_attribute('textContent')
+                sml['申请日'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[6]/td[2]').get_attribute('textContent')
+                sml['初审公告日'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[6]/td[4]').get_attribute('textContent')
+                sml['注册公告日'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[7]/td[2]').get_attribute('textContent')
+                sml['专用期限'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[7]/td[4]').get_attribute('textContent')
+                sml['专利代理机构'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[8]/td[2]').get_attribute('textContent')
+                sml['商标公告'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[9]/td[2]').get_attribute('textContent')
+                sml['商品服务项目'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[11]/td[1]').get_attribute('textContent')
+                close = drive.find_element_by_xpath('//div[@class="modal-header modal-new-header"]/div[@class="close-div"]')
+                drive.execute_script('arguments[0].click()', close)
+                a.append({i: {'商标图片': sml['商标图片'], '商标名称': sml['商标名称'], '商标注册号': sml['商标注册号'], '商标类别': sml['商标类别'],'商标状态': sml['商标状态'], '申请人': sml['申请人'], '申请日': sml['申请日'], '初审公告日': sml['初审公告日'],'注册公告日': sml['注册公告日'], '专用期限': sml['专用期限'], '专利代理机构': sml['专利代理机构'], '商标公告': sml['商标公告'],'商品服务项目': sml['商品服务项目']}})
+                i += 1
+            print(a)
+            items['商标'] = a
+            yield items
 
         #专利信息
-        urls = response.xpath('//div[@class="container"]/div[@class="row"]/div[@class="col-md-18"]/div[@class="tab-content"]/table[@class="table table-bordered margin-t-1x"]/tbody/tr/td/h5[@class="h5"]/a/@href').extract()
-        # print(urls)
-        i=0
-        url = list('https://www.qixin.com'+urls[i].format(x) for x in range(6))
-        print(url)
-        i+=1
-        yield scrapy.Request(url=url,callback=self.parse)
+        # urls = response.xpath('//div[@class="container"]/div[@class="row"]/div[@class="col-md-18"]/div[@class="tab-content"]/table[@class="table table-bordered margin-t-1x"]/tbody/tr/td/h5[@class="h5"]/a/@href').extract()
+        # # print(urls)
+        # i=0
+        # url = list('https://www.qixin.com'+urls[i].format(x) for x in range(6))
+        # print(url)
+        # i+=1
+        # yield scrapy.Request(url=url,callback=self.parse)
 
 
         #著作权
@@ -65,5 +107,4 @@ class TrademarkSpider(scrapy.Spider):
         #资质认证
 
 
-        drive.close()
-        yield items
+            drive.close()
