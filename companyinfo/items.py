c03e5da2056888e6a2d84e784ba25aabcd16bccd
Stervger <1406421751@qq.com>
2019-10-28 21:28:08 +0800

    2019/10/28 ability finish

--- companyinfo/items.py
+++ companyinfo/items.py
@@ -10,92 +10,7 @@ import scrapy
 
 class CompanyinfoItem(scrapy.Item):
     #基本信息
-    dc_id = scrapy.Field()  # 主键
-    data_version_id = scrapy.Field()  # 版本
-    licence_pro = scrapy.Field()  # 许可经营项目
-    ind_econ_code = scrapy.Field()  # 国民经济行业代码
-    ind_econ_name = scrapy.Field()  # 国民经济代码名称
-    source_type = scrapy.Field()  # 类型（0:il_annualreport、1:annualreport)
-    oreg_no = scrapy.Field()  # 原注册号
-    reg_no = scrapy.Field()  # 企业注册号
-    firm_name = scrapy.Field()  # 企业名称简称
-    firm_name_en = scrapy.Field()  # 企业英文名称
-    firm_name_sh = scrapy.Field()  # 企业名称简称
-    firm_name_used = scrapy.Field()  # 企业曾用名
-    firm_type_code = scrapy.Field()  # 企业类型编码
-    firm_type = scrapy.Field()  # 企业类型
-    org_code = scrapy.Field()  # 组织机构代码
-    uni_cre_code = scrapy.Field()  # 统一信用代码
-    postalcode = scrapy.Field()  # 企业邮政编码
-    legal_rep = scrapy.Field()  # 法定代表人
-    reg_institution = scrapy.Field()  # 登记机关
-    est_date = scrapy.Field()  # 成立日期
-    cancel_date = scrapy.Field()  # 注销日期
-    appr_date = scrapy.Field()  # 核准日期
-    rev_date = scrapy.Field()  # 吊销日期
-    open_date = scrapy.Field()  # 营业开始日期
-    close_date = scrapy.Field()  # 营业结束日期
-    province = scrapy.Field()  # 省份
-    city = scrapy.Field()  # 城市
-    district = scrapy.Field()  # 区/县
-    final_check_year = scrapy.Field()  # 最后年检年度
-    con_email = scrapy.Field()  # 联系邮箱
-    con_addr = scrapy.Field()  # 地址
-    con_tel = scrapy.Field()  # 联系电话
-    addr_district = scrapy.Field()  # 住所所在行政区
-    reg_addr_code = scrapy.Field()  # 注册地址行政编号
-    reg_cap = scrapy.Field()  # 注册资本
-    reg_cap_cur = scrapy.Field()  # 注册资本币种
-    rec_cap = scrapy.Field()  # 实收资本(万元)
-    ope_status = scrapy.Field()  # 经营状态
-    ope_scope = scrapy.Field()  # 经营业务范围
-    emp_num = scrapy.Field()  # 员工人数
-    ann_report_id = scrapy.Field()  # 年报ID
-    ann_report_date = scrapy.Field()  # 年报日期
-    ann_report_year = scrapy.Field()  # 年报年份
-    release_date = scrapy.Field()  # 发布日期
-    ins_num_pen = scrapy.Field()  # 城镇职工基本养老保险参保人数
-    ins_num_unemp = scrapy.Field()  # 失业保险参保人数
-    ins_num_med = scrapy.Field()  # 职工基本医疗保险参保人数
-    ins_num_injury = scrapy.Field()  # 工伤保险参保人数
-    ins_num_rear = scrapy.Field()  # 生育保险参保人数
-    ins_real_pay_pen = scrapy.Field()  # 参加城镇职工基本养老保险本期实际缴费金额
-    ins_real_pay_unemp = scrapy.Field()  # 参加失业保险本期实际缴费金额
-    ins_real_pay_med = scrapy.Field()  # 参加职工基本医疗保险本期实际缴费金额
-    ins_real_pay_injury = scrapy.Field()  # 参加工伤保险本期实际缴费金额
-    ins_real_pay_rear = scrapy.Field()  # 参加生育保险本期实际缴费金额
-    ins_base_pen = scrapy.Field()  # 单位参加城镇职工基本养老保险缴费基数
-    ins_base_unemp = scrapy.Field()  # 单位参加失业保险缴费基数
-    ins_base_med = scrapy.Field()  # 单位参加职工基本医疗保险缴费基数
-    ins_base_injury = scrapy.Field()  # 单位参加工伤保险缴费基数
-    ins_base_rear = scrapy.Field()  # 单位参加生育保险缴费基数
-    ins_unpaid_city = scrapy.Field()  # 单位参加城镇职工基本养老保险累计欠缴金额
-    ins_unpaid_unemp = scrapy.Field()  # 单位参加失业保险累计欠缴金额
-    ins_unpaid_med = scrapy.Field()  # 单位参加职工基本医疗保险累计欠缴金额
-    ins_unpaid_injury = scrapy.Field()  # 参加工伤保险累计欠缴金额
-    ins_unpaid_rear = scrapy.Field()  # 单位参加生育保险累计欠缴金额
-    whether_website = scrapy.Field()  # 是否有网站或网店
-    web_addr = scrapy.Field()  # 网站地址
-    web_name = scrapy.Field()  # 网站名称
-    web_type = scrapy.Field()  # 网站类型
-    web_source = scrapy.Field()  # 网址来源
-    domain_addr = scrapy.Field()  # 域名网址
-    domain_name = scrapy.Field()  # 域名名称
-    domain_permit_num = scrapy.Field()  # 域名许可证号
-    domain_date = scrapy.Field()  # 域名登记批准日期
-    liquidation_debt_receiver = scrapy.Field()  # 清算债务承接人
-    liquidation_credit_receiver = scrapy.Field()  # 清算债权承接人
-    liquidation_principal = scrapy.Field()  # 清算负责人
-    liquidation_member = scrapy.Field()  # 清算组成员
-    liquidation_start_date = scrapy.Field()  # 清算开始日期
-    liquidation_end_date = scrapy.Field()  # 清算完结日期
-    liquidation_period = scrapy.Field()  # 清算时长
-    liquidation_end_details = scrapy.Field()  # 清算完结情况
-    liquidation_type = scrapy.Field()  # 清算类型
-    liquidation_prop_amt = scrapy.Field()  # 清算财产数额
-    liquidation_valu_method = scrapy.Field()  # 清算财产计价方法
-    liquidation_cost = scrapy.Field()  # 清算费用
-    datachange_lasttime = scrapy.Field()  # 获取时间
+
 
 ##########################################################################
     #商标（trademark）
@@ -132,3 +47,60 @@ class CompanyinfoItem(scrapy.Item):
     创作完成日期 = scrapy.Field()
     登记日期 = scrapy.Field()
     首次发布日期 = scrapy.Field()
+
+###########################################################################
+    #软件著作权
+    软件著作权 = scrapy.Field()
+    软件名称 = scrapy.Field()
+    登记号 = scrapy.Field()
+    版本号 = scrapy.Field()
+    分类号 = scrapy.Field()
+    登记批准日期 = scrapy.Field()
+    软件简称 = scrapy.Field()
+
+##########################################################################
+    #资质认证
+    资质认证 = scrapy.Field()
+    发证日期 = scrapy.Field()
+    证书类别 = scrapy.Field()
+    截止日期 = scrapy.Field()
+    证书编号 = scrapy.Field()
+    状态 = scrapy.Field()
+    备注 = scrapy.Field()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- companyinfo/spiders/trademark.py
+++ companyinfo/spiders/trademark.py
@@ -13,7 +13,7 @@ class TrademarkSpider(scrapy.Spider):
     name = 'trademark'
     start_urls = {'https://www.qixin.com/ability/60214084-2b87-4ea3-9131-e4c313deacf0'}
     def parse(self, response):
-        drive = webdriver.Chrome()
+        drive = webdriver.Firefox()
         drive.get("https://www.qixin.com/ability/60214084-2b87-4ea3-9131-e4c313deacf0")
         items = CompanyinfoItem()
 
@@ -41,11 +41,11 @@ class TrademarkSpider(scrapy.Spider):
         #             time.sleep(2)
         #             trademark_detail['商标图片'] = drive.find_element_by_xpath('//img[@class="img-responsive img-center"]').get_attribute('src')
         #             trademark_detail['商标名称'] = drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr/td[@class="td-2"]').get_attribute('textContent')
-        #             strademark_detailml['商标注册号'] = drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr[2]/td[2]').get_attribute('textContent')
+        #             trademark_detail['商标注册号'] = drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr[2]/td[2]').get_attribute('textContent')
         #             trademark_detail['商标类别'] = drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr[3]/td[2]').get_attribute('textContent')
         #             trademark_detail['商标状态'] = drive.find_element_by_xpath('//div[@class="modal-content"]/div[@class="modal-body padding-t-0x"]/div[@class="row"]/div[@class="col-xs-24"]/table[@class="table table1 table-bordered"]/tbody/tr[4]/td[2]').get_attribute('textContent')
         #             trademark_detail['申请人'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[5]/td[2]').get_attribute('textContent')
-        #             smtrademark_detaill['申请日'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[6]/td[2]').get_attribute('textContent')
+        #             trademark_detail['申请日'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[6]/td[2]').get_attribute('textContent')
         #             trademark_detail['初审公告日'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[6]/td[4]').get_attribute('textContent')
         #             trademark_detail['注册公告日'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[7]/td[2]').get_attribute('textContent')
         #             trademark_detail['专用期限'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/div/div/table/tbody/tr[7]/td[4]').get_attribute('textContent')
@@ -88,7 +88,6 @@ class TrademarkSpider(scrapy.Spider):
         #     print(trademark)
         #     items['商标'] = trademark
         #     yield items
-        #     drive.close()
 
         #专利信息
         # urls = response.xpath('//div[@class="container"]/div[@class="row"]/div[@class="col-md-18"]/div[@class="tab-content"]/table[@class="table table-bordered margin-t-1x"]/tbody/tr/td/h5[@class="h5"]/a/@href').extract()
@@ -200,10 +199,112 @@ class TrademarkSpider(scrapy.Spider):
 
 
         #软件著作权
-
+        # totalpage = int(drive.find_element_by_xpath('//*[@id="copyRightSoft"]/h4/span').get_attribute('textContent')) // 5 + 1
+        # print(totalpage)
+        # if totalpage != 1:
+        #     for p in range(1, totalpage):
+        #         if p <= 3:
+        #             p = p
+        #         elif p == totalpage:
+        #             p = 5
+        #         else:
+        #             p = 4
+        #         page = drive.find_element_by_xpath('//*[@id="copyRightSoft"]/div/div/nav/ul/li[{0}]/a'.format(p + 1))
+        #         drive.execute_script('arguments[0].click()', page)
+        #         time.sleep(2)
+        #         copyRightSoft_detail = {}
+        #         i = 1
+        #         copyRightSoft = []
+        #         tr_list = response.xpath('//*[@id="copyRightSoft"]/table/tbody/tr')
+        #         for tr in tr_list:
+        #             time.sleep(2)
+        #             copyRightSoft_detail['软件名称'] = drive.find_element_by_xpath('//*[@id="copyRightSoft"]/table/tbody/tr[{0}]/td[2]'.format(i)).get_attribute('textContent')
+        #             copyRightSoft_detail['登记号'] = drive.find_element_by_xpath('//*[@id="copyRightSoft"]/table/tbody/tr[{0}]/td[3]'.format(i)).get_attribute('textContent')
+        #             copyRightSoft_detail['版本号'] = drive.find_element_by_xpath('//*[@id="copyRightSoft"]/table/tbody/tr[{0}]/td[4]'.format(i)).get_attribute('textContent')
+        #             copyRightSoft_detail['分类号'] = drive.find_element_by_xpath('//*[@id="copyRightSoft"]/table/tbody/tr[{0}]/td[5]'.format(i)).get_attribute('textContent')
+        #             copyRightSoft_detail['登记批准日期'] = drive.find_element_by_xpath('//*[@id="copyRightSoft"]/table/tbody/tr[{0}]/td[6]'.format(i)).get_attribute('textContent')
+        #             copyRightSoft_detail['软件简称'] = drive.find_element_by_xpath('//*[@id="copyRightSoft"]/table/tbody/tr[{0}]/td[7]'.format(i)).get_attribute('textContent')
+        #             copyRightSoft.append({i: {'软件名称': copyRightSoft_detail['软件名称'], '登记号': copyRightSoft_detail['登记号'], '版本号': copyRightSoft_detail['版本号'], '分类号': copyRightSoft_detail['分类号'],'登记批准日期': copyRightSoft_detail['登记批准日期'], '软件简称': copyRightSoft_detail['软件简称']}})
+        #             i += 1
+        #         print(copyRightSoft)
+        #         items['软件著作权'] = copyRightSoft
+        #         yield items
+        # else:
+        #     copyRightSoft_detail = {}
+        #     i = 1
+        #     copyRightSoft = []
+        #     tr_list = response.xpath('//*[@id="copyRightSoft"]/table/tbody/tr')
+        #     for tr in tr_list:
+        #         time.sleep(2)
+        #         copyRightSoft_detail['软件名称'] = drive.find_element_by_xpath('//*[@id="copyRightSoft"]/table/tbody/tr[{0}]/td[2]'.format(i)).get_attribute('textContent')
+        #         copyRightSoft_detail['登记号'] = drive.find_element_by_xpath('//*[@id="copyRightSoft"]/table/tbody/tr[{0}]/td[3]'.format(i)).get_attribute('textContent')
+        #         copyRightSoft_detail['版本号'] = drive.find_element_by_xpath('//*[@id="copyRightSoft"]/table/tbody/tr[{0}]/td[4]'.format(i)).get_attribute('textContent')
+        #         copyRightSoft_detail['分类号'] = drive.find_element_by_xpath('//*[@id="copyRightSoft"]/table/tbody/tr[{0}]/td[5]'.format(i)).get_attribute('textContent')
+        #         copyRightSoft_detail['登记批准日期'] = drive.find_element_by_xpath('//*[@id="copyRightSoft"]/table/tbody/tr[{0}]/td[6]'.format(i)).get_attribute('textContent')
+        #         copyRightSoft_detail['软件简称'] = drive.find_element_by_xpath('//*[@id="copyRightSoft"]/table/tbody/tr[{0}]/td[7]'.format(i)).get_attribute('textContent')
+        #         copyRightSoft.append({i: {'软件名称': copyRightSoft_detail['软件名称'], '登记号': copyRightSoft_detail['登记号'], '版本号': copyRightSoft_detail['版本号'], '分类号': copyRightSoft_detail['分类号'],'登记批准日期': copyRightSoft_detail['登记批准日期'], '软件简称': copyRightSoft_detail['软件简称']}})
+        #         i += 1
+        #     print(copyRightSoft)
+        #     items['软件著作权'] = copyRightSoft
+        #     yield items
 
         #资质认证
-
+        totalpage = int(drive.find_element_by_xpath('//*[@id="certificate"]/h4/span').get_attribute('textContent'))//5 + 1
+        print(totalpage)
+        if totalpage!=1:
+            for p in range(1,totalpage):
+                if p <= 3:
+                    p=p
+                elif p==totalpage:
+                    p=5
+                else:
+                    p=4
+                page = drive.find_element_by_xpath('//*[@id="certificate"]/div/div/nav/ul/li[{0}]/a'.format(p+1))
+                drive.execute_script('arguments[0].click()', page)
+                time.sleep(2)
+                certificate_detail = {}
+                i = 1
+                certificate = []
+                tr_list = response.xpath('//*[@id="certificate"]/table/tbody/tr')
+                for tr in tr_list:
+                    detail = drive.find_element_by_xpath('//*[@id="certificate"]/table/tbody/tr[{0}]/td[5]/div/a'.format(i))
+                    drive.execute_script('arguments[0].click()', detail)
+                    time.sleep(2)
+                    certificate_detail['发证日期'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/table/tbody/tr[1]/td[2]').get_attribute('textContent')
+                    certificate_detail['证书类别'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/table/tbody/tr[2]/td[2]').get_attribute('textContent')
+                    certificate_detail['截止日期'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/table/tbody/tr[2]/td[4]').get_attribute('textContent')
+                    certificate_detail['证书编号'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/table/tbody/tr[3]/td[2]').get_attribute('textContent')
+                    certificate_detail['状态'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/table/tbody/tr[3]/td[4]').get_attribute('textContent')
+                    certificate_detail['备注'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/table/tbody/tr[4]/td[2]').get_attribute('textContent')
+                    close = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[1]/div[1]')
+                    drive.execute_script('arguments[0].click()', close)
+                    certificate.append({i: {'发证日期': certificate_detail['发证日期'], '证书类别': certificate_detail['证书类别'], '截止日期': certificate_detail['截止日期'],'证书编号': certificate_detail['证书编号'], '状态': certificate_detail['状态'], '备注': certificate_detail['备注']}})
+                    i += 1
+                print(certificate)
+                items['资质认证'] = certificate
+                yield items
+        else:
+            certificate_detail = {}
+            i = 1
+            certificate = []
+            tr_list = response.xpath('//*[@id="certificate"]/table/tbody/tr')
+            for tr in tr_list:
+                detail = drive.find_element_by_xpath('//*[@id="certificate"]/table/tbody/tr[{0}]/td[5]/div/a'.format(i))
+                drive.execute_script('arguments[0].click()', detail)
+                time.sleep(2)
+                certificate_detail['发证日期'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/table/tbody/tr[1]/td[2]').get_attribute('textContent')
+                certificate_detail['证书类别'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/table/tbody/tr[2]/td[2]').get_attribute('textContent')
+                certificate_detail['截止日期'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/table/tbody/tr[2]/td[4]').get_attribute('textContent')
+                certificate_detail['证书编号'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/table/tbody/tr[3]/td[2]').get_attribute('textContent')
+                certificate_detail['状态'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/table/tbody/tr[3]/td[4]').get_attribute('textContent')
+                certificate_detail['备注'] = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[2]/table/tbody/tr[4]/td[2]').get_attribute('textContent')
+                close = drive.find_element_by_xpath('/html/body/div[10]/div/div/div[1]/div[1]')
+                drive.execute_script('arguments[0].click()', close)
+                certificate.append({i: {'发证日期': certificate_detail['发证日期'], '证书类别': certificate_detail['证书类别'], '截止日期': certificate_detail['截止日期'],'证书编号': certificate_detail['证书编号'], '状态': certificate_detail['状态'], '备注': certificate_detail['备注']}})
+                i += 1
+            print(certificate)
+            items['资质认证'] = certificate
+            yield items
 
 
         drive.close()
